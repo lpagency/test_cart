d191634c5292ac9ea39dd1de07c6aaee89d99fc3
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-01-15 11:04:22 -0300

    test

---
+++ static/js/json.data.js
@@ -0,0 +1,232 @@
+var categories =[
+{
+	"name": "Todo",
+	"order": 0,
+	"tag": "",
+	"id": 0,
+	"type": "edad"
+},
+{
+	"name": "0-6 meses",
+	"tag": "0_6_meses",
+	"order": 1,
+	"id": 1,
+	"type": "edad",
+	"info": "En esta etapa es muy importante estimular los sentidos y la motricidad. Para esto te recomendamos mostrarle nuevas experiencias que activen sus sentidos y lo ayuden a conocer y ejecutar sus distintas capacidades motrices para que se descubra a sí mismo y las múltiples formas en que puede actuar sobre el medio."
+},	
+{
+	"name": "6-12 meses",
+	"tag": "6_12_meses",
+	"order": 2,
+	"id": 2,
+	"type": "edad",
+	"info": "En esta etapa una de sus principales actividades será la de manipular objetos. Será capaz de coordinar sus manos acercando los objetos a su boca y se sentirá atraído por los objetos sonoros . Además, aprenderá a moverse por sí mismo y a ejercer control sobre los juguetes al ser capaz de mantenerse sentado."
+},	
+{
+	"name": "12-18 meses",
+	"tag": "12_18_meses",
+	"order": 3,
+	"id": 3,
+	"type": "edad",
+	"info": "Esta etapa  adquirirá una mayor movilidad y autosuficiencia a nivel motor y la inteligencia sensorio motriz le permitirá manipular objetos y explorar el espacio. Irá probando y ensayando cada vez que se encuentre con una situación distinta, de esta forma, será capaz de descubrir nuevos esquemas que darán solución a las situaciones en las que se encuentre."
+},	
+{
+	"name": "18-24 meses",
+	"tag": "18_24_meses",
+	"order": 4,
+	"id": 4,
+	"type": "edad",
+	"info": "En esta etapa conseguirá una movilidad completa que le dará nuevas posibilidades de exploración con respecto a su entorno y una mayor independencia. Además, a nivel cognitivo adquirirá la madurez necesaria para poseer esquemas de representación. De este modo, el pensamiento actuará cada vez más con representaciones, anticipando los efectos sin necesidad de probarlos."
+},	
+{
+	"name": "2-3 años",
+	"tag": "2_3_anos",
+	"order": 5,
+	"id": 5,
+	"type": "edad",
+	"info": "En este periodo, los niños entran a la etapa pre operacional,  es aquí cuando aprenden como interactuar con su ambiente mediante el uso  de palabras y de imágenes mentales, sin tener necesidad de que estén presente ,y por lo tanto anticiparse a la acción  con su pensamiento. La etapa de los 2 a 3 años es una fase de la búsqueda de la autonomía,  dónde además comienzan a combinar palabras y a construir frases."
+},	
+{
+	"name": "3-4 años",
+	"tag": "3_4_anos",
+	"order": 6,
+	"id": 6,
+	"type": "edad",
+	"info": "Sus habilidades motrices están más desarrolladas, en general su lenguaje es más fluido y adquiere mayor autonomía e independencia al realizar por sí mismo tareas y actividades, especialmente a través del juego. A nivel cognitivo,  tiene períodos de concentración más largos , por lo tanto mejora su memoria, que será imprescindible para futuros aprendizajes."
+},	
+{
+	"name": "4-5 años",
+	"tag": "4_5_anos",
+	"order": 7,
+	"id": 7,
+	"type": "edad",
+	"info": "Lo común en estas edades es que los niños muestren progresivamente mayores destrezas corporales e intelectuales, creciendo en cada área de su desarrollo.  En esta etapa los niños se manifiestan interesados en todo lo que les rodea queriendo buscar el porqué de las cosas, y cómo ocurren etc. "
+},	
+{
+	"name": "5-6 años",
+	"tag": "5_6_anos",
+	"order": 8,
+	"id": 8,
+	"type": "edad",
+	"info": "En esta etapa presentan un rápido aprendizaje. Intelectualmente están más maduros y pueden prestar atención por mas tiempo, así como seguir el hilo de una narración. A esta edad los niños ya comprenden perfectamente las relaciones causa – efecto, lo que les confiere una lógica mucho más coherente"
+},	
+{
+	"name": "Todo",
+	"tag": "",
+	"order": 9,
+	"id": 9,
+	"type": "area",
+	"info": ""
+},	
+{
+	"name": "Afectivo/Social",
+	"tag": "afectivo_social",
+	"order": 10,
+	"id": 10,
+	"type": "area",
+	"info": "Esta área incluye todas las experiencias afectivas, de independencia  y el proceso de la socialización del niño, que le permitirá sentirse confiado, seguro y capaz de relacionarse  de acuerdo a las normas comunes."
+},	
+{
+	"name": "Cognitivo",
+	"tag": "cognitivo",
+	"order": 11,
+	"id": 11,
+	"type": "area",
+	"info": "Le permitirá al niño comprender, relacionar, adaptarse a nuevas situaciones, haciendo uso del pensamiento y la interacción directa con los objetos y el mundo que lo rodea. Estimula sus capacidades intelectuales."
+},	
+{
+	"name": "Lenguaje",
+	"tag": "lenguaje",
+	"order": 12,
+	"id": 12,
+	"type": "area",
+	"info": "Está referida a las habilidades que le permitirán al niño comunicarse con su entorno y abarca tres aspectos: La capacidad comprensiva, la capacidad expresiva y la capacidad gestual. "
+},	
+{
+	"name": "Motricidad Fina",
+	"tag": "motricidad_fina",
+	"order": 13,
+	"id": 13,
+	"type": "area",
+	"info": "La finalidad es poder lograr que el niño pueda tener mejor precisión y coordinación de movimientos manuales. Coordinación entre lo que ve y toca, lo que le servirá posteriormente para desenvolverse adecuadamente en el mundo escolar (ej. escritura)."
+},	
+{
+	"name": "Motricidad Gruesa",
+	"tag": "motricidad_gruesa",
+	"order": 14,
+	"id": 14,
+	"type": "area",
+	"info": "La finalidad de estimular esta área es para que el niño pueda ir aprendiendo a coordinar las partes de su cuerpo y mantenga el equilibrio, pudiendo así  explorar libremente."
+},
+{
+	"name": "Todo",
+	"tag": "",
+	"order": 15,
+	"id": 15,
+	"type": "marca"
+},
+{
+	"name": "Dactic",
+	"tag": "dactic",
+	"order": 16,
+	"id": 16,
+	"type": "marca"
+},	
+{
+	"name": "Dantoy",
+	"tag": "dantoy",
+	"order": 17,
+	"id": 17,
+	"type": "marca"
+},	
+{
+	"name": "Djeco",
+	"tag": "djeco",
+	"order": 18,
+	"id": 18,
+	"type": "marca"
+},	
+{
+	"name": "Evaflex",
+	"tag": "evaflex",
+	"order": 19,
+	"id": 19,
+	"type": "marca"
+},	
+{
+	"name": "Gowi",
+	"tag": "gowi",
+	"order": 20,
+	"id": 20,
+	"type": "marca"
+},	
+{
+	"name": "Imanix",
+	"tag": "imanix",
+	"order": 21,
+	"id": 21,
+	"type": "marca"
+},	
+{
+	"name": "Roylco",
+	"tag": "roylco",
+	"order": 22,
+	"id": 22,
+	"type": "marca"
+},	
+{
+	"name": "Scotchi",
+	"tag": "scotchi",
+	"order": 23,
+	"id": 23,
+	"type": "marca"
+},	
+{
+	"name": "Taf Toys",
+	"tag": "taf_toys",
+	"order": 24,
+	"id": 24,
+	"type": "marca"
+},	
+{
+	"name": "Alex",
+	"tag": "alex",
+	"order": 25,
+	"id": 25,
+	"type": "marca"
+},
+{
+	"name": "Slinky",
+	"tag": "slinky",
+	"order": 26,
+	"id": 26,
+	"type": "marca"
+},
+{
+	"name": "Crocodile",
+	"tag": "crocodile",
+	"order": 27,
+	"id": 27,
+	"type": "marca"
+},
+{
+	"name": "Hape",
+	"tag": "hape",
+	"order": 28,
+	"id": 28,
+	"type": "marca"
+},
+{
+	"name": "Swimava",
+	"tag": "swimava",
+	"order": 29,
+	"id": 29,
+	"type": "marca"
+},
+{
+	"name": "Edushapes",
+	"tag": "edushape",
+	"order": 30,
+	"id": 30,
+	"type": "marca"
+}];
